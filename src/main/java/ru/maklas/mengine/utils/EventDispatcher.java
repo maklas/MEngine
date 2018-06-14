3562beb84bb8890a13885d118865f9ab3d4b6caf
maklas <maklas-a@yandex.ru>
2018-06-14 12:34:52 +0300

    Can obtain current events from eventDispatcher at any time

--- src/main/java/ru/maklas/mengine/utils/EventDispatcher.java
+++ src/main/java/ru/maklas/mengine/utils/EventDispatcher.java
@@ -1,11 +1,13 @@
 package ru.maklas.mengine.utils;
 
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ObjectMap;
 import ru.maklas.mengine.Subscription;
 
 public class EventDispatcher {
 
     private final ObjectMap<Class, Signal> map = new ObjectMap<Class, Signal>();
+    private final Array<Object> eventStack = new Array<Object>();
 
     @SuppressWarnings("all")
     public <T> void subscribe(Subscription<T> subscription){
@@ -29,7 +31,63 @@ public class EventDispatcher {
     public void dispatch(Object event){
         Signal signal = map.get(event.getClass());
         if (signal != null){
+            eventStack.add(event);
             signal.dispatch(event);
+            eventStack.pop();
+        }
+    }
+
+    /**
+     * Size of event stack. All events that are being fired at this moment.
+     * Latest are on top
+     */
+    public int stackSize(){
+        return eventStack.size;
+    }
+
+    /**
+     * Root event that has been fired first on the stack.
+     * Might throw an Exception if stackSize() == 0
+     */
+    public Object firstEvent(){
+        return eventStack.first();
+    }
+
+    /**
+     * Event that's being fired right now. Might throw Exception if stack's size is 0
+     */
+    public Object currentEvent(){
+        return eventStack.peek();
+    }
+
+    /**
+     * <p>
+     * Use positive index to get events from the stack starting from the bottom
+     * </p>
+     * <p>
+     * Use negative index to trace events back from current.
+     * </p>
+     * <p>
+     *     Example:
+     *
+     *     <li>
+     *         index = 0. Will return firstEvent()
+     *     </li>
+     *     <li>
+     *         index = 1. Will return second event that was dispatched during firstEvent()
+     *     </li>
+     *     <li>
+     *         index = -1. Will return the event that was dispatched right before currentEvent().
+     *     </li>
+     * </p>
+     *
+     * @throws RuntimeException if abs(index) >= stackSize()!
+     */
+    public Object getEvent(int index){
+        if (index >= 0){
+            return eventStack.get(index);
+        } else {
+            return eventStack.get(eventStack.size - 1 + index);
         }
     }
 
